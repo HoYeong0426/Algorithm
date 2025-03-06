e7c176b7cee09d06fd2c0dd980fb3fbd97673b30
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-03-06 12:00:42 +0900

    [Silver I] Title: 신입 사원, Time: 2712 ms, Memory: 325136 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,40 +1,41 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-        while (T-- > 0) {
-            int N = Integer.parseInt(br.readLine()); 
-            int[][] applicants = new int[N][2];
+        int N = Integer.parseInt(br.readLine());
 
-            for (int i = 0; i < N; i++) {
+        while (N-- > 0) {
+            int M = Integer.parseInt(br.readLine());
+
+            int[][] arr = new int[M][2];
+
+            for (int i = 0; i < M; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                applicants[i][0] = Integer.parseInt(st.nextToken());
-                applicants[i][1] = Integer.parseInt(st.nextToken());
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(applicants, (a, b) -> Integer.compare(a[0], b[0]));
+            Arrays.sort(arr, (o1,o2) -> o1[0] - o2[0]);
 
             int count = 1;
-            int bestInterviewRank = applicants[0][1]; 
+            int other = arr[0][1];
 
-            for (int i = 1; i < N; i++) {
-                if (applicants[i][1] < bestInterviewRank) { 
+            for (int i = 1; i < M; i ++) {
+                if (arr[i][1] < other) {
                     count++;
-                    bestInterviewRank = applicants[i][1]; 
+                    other = arr[i][1];
                 }
             }
 
-            sb.append(count).append("\n");
+            System.out.println(count);
+
         }
 
-        System.out.print(sb);
     }
 }
