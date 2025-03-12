d03cc20dbaca7b6540f7b29870f12ccbdf444bac
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-03-12 18:15:02 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,26 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int T = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
         int[] dp = new int[11];
-        
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-        
+
         for (int i = 4; i <= 10; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
         }
-
-        for (int t = 0; t < T; t++) {
-            int n = sc.nextInt();
-            System.out.println(dp[n]);
+        
+        while (N-- > 0) {
+            int M = Integer.parseInt(br.readLine());
+            System.out.println(dp[M]);
         }
-
-        sc.close();
+        
     }
 }
