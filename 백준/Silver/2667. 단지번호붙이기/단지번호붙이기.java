f927c2c15e2aae0ca4c84351ba3f34faefc686bf
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-03-20 15:31:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,54 +1,81 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main{
-    static int N, count;
+public class Main {
+
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
     static int[][] map;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    
-    public static void dfs(int x, int y){
-        map[x][y] = 0;
-        count += 1;
-        
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(nx>=0 && nx<N && ny>=0 && ny<N && map[nx][ny]==1) dfs(nx, ny);
-        }
-    }
-    
-    public static void main(String args[]) throws IOException{
+    static boolean[][] visited;
+    static int count;
+    static ArrayList<Integer> result = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        N = Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+
         map = new int[N][N];
-        
-        for(int i=0; i<N; i++){
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
             String str = br.readLine();
-            for(int j=0; j<N; j++){
+            for (int j = 0; j < N; j++) {
                 map[i][j] = str.charAt(j) - '0';
             }
         }
-        
-        ArrayList<Integer> arr = new ArrayList<>();
-        int result = 0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                count = 0;
-                if(map[i][j] == 1){
-                    dfs(i,j);
-                    arr.add(count);
-                    result++;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    bfs(i, j);
+                    count++;
                 }
+                
             }
         }
-        
-        Collections.sort(arr);
-        System.out.println(result);
-        for(int i : arr){
-            System.out.println(i);
+
+        System.out.println(count);
+        Collections.sort(result);
+        for (Integer num : result) {
+            System.out.println(num);
         }
     }
+
+
+    static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {x, y});
+        visited[x][y] = true;
+        int cnt = 1;
+
+        while (!queue.isEmpty()) {
+            int[] arr = queue.poll();
+            int nowX = arr[0];
+            int nowY = arr[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= map.length || nextY >= map.length) continue;
+                if (visited[nextX][nextY] || map[nextX][nextY] == 0) continue;
+
+                queue.offer(new int[] {nextX, nextY});
+                visited[nextX][nextY] = true;
+                cnt++;
+            }
+        }
+
+        result.add(cnt);
+        
+    }
+
 }
