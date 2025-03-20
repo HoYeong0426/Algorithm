cb064e1c15bd70776751f37e5b06f6acddef1bca
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-03-20 18:39:51 +0900

    [Gold V] Title: 토마토, Time: 628 ms, Memory: 120704 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,67 +1,82 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static int[][] map;
+    static int days;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int M = sc.nextInt();
-        int N = sc.nextInt();
-        int[][] box = new int[N][M];
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        map = new int[x][y];
         Queue<int[]> queue = new LinkedList<>();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                box[i][j] = sc.nextInt();
-                if (box[i][j] == 1) {
-                    queue.add(new int[]{i, j});
-                } 
+        for (int i = 0; i < x; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < y; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) queue.offer(new int[] {i, j});
             }
         }
 
-        int days = bfs(box, queue, N, M);
+        bfs(queue);
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (box[i][j] == 0) {
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if (map[i][j] == 0) {
                     System.out.println(-1);
                     return;
                 }
             }
         }
 
-        System.out.println(days);
+        System.out.println(days - 1);
+
+
     }
 
-    static int bfs(int[][] box, Queue<int[]> queue, int N, int M) {
-        int days = -1;
+    static void bfs(Queue<int[]> queue) {
+        days = 0;
 
         while (!queue.isEmpty()) {
+
             int size = queue.size();
 
             for (int i = 0; i < size; i++) {
-                int[] current = queue.poll();
-                int x = current[0];
-                int y = current[1];
+                int[] arr = queue.poll();
+                int nowX = arr[0];
+                int nowY = arr[1];
 
-                for (int d = 0; d < 4; d++) {
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
+                for (int k = 0; k < 4; k++) {
+                    int nextX = nowX + dx[k];
+                    int nextY = nowY + dy[k];
 
-                    if (nx >= 0 && ny >= 0 && nx < N && ny < M && box[nx][ny] == 0) {
-                        box[nx][ny] = 1;
-                        queue.add(new int[]{nx, ny});
+                    if (nextX >= 0 && nextY >= 0 && nextX < map.length && nextY < map[0].length && map[nextX][nextY] == 0) {
+                        queue.offer(new int[] {nextX, nextY});
+                        map[nextX][nextY] = 1;
                     }
                 }
             }
 
             days++;
+
         }
 
-        return days;
     }
+
+
 }
