45bcdecb856c6721a69c17a21f1dc38d4a0a0e53
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-03-20 19:51:55 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 23304 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -5,35 +5,37 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        
-        System.out.println(bfs(N, K));
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(a, b));
     }
-    
-    static int bfs(int start, int target) {
-        if (start == target) return 0;
-        
-        int[] visited = new int[100001];
+
+
+    static int bfs(int a, int b) {
+        if (a == b) return 0;
+
         Queue<Integer> queue = new LinkedList<>();
-        
-        queue.offer(start);
-        visited[start] = 0;
-        
+        queue.offer(a);
+
+        int[] visited = new int[100001];
+
         while (!queue.isEmpty()) {
-            int current = queue.poll();
-            
-            for (int next : new int[]{current - 1, current + 1, current * 2}) {
+            int now = queue.poll();
+
+            for (int next : new int[] {now - 1, now + 1, now * 2}) {
                 if (next >= 0 && next <= 100000 && visited[next] == 0) {
-                    visited[next] = visited[current] + 1;
                     queue.offer(next);
-                    
-                    if (next == target) return visited[next];
+                    visited[next] = visited[now] + 1;
+                    if (next == b) return visited[next];
                 }
+
             }
+            
         }
-        
+
         return -1;
     }
+    
 }
