4a82f4eacbbf55d5c11b876453fa635b92588fdb
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-03-28 16:41:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 544 ms, Memory: 61240 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -5,43 +5,47 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
-        List<Integer>[] tree = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            tree[i] = new ArrayList<>();
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
-        
+
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            tree[a].add(b);
-            tree[b].add(a);
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
+
+        int[] parent = bfs(N, graph);
         
-        int[] parent = new int[N + 1];
-        boolean[] visited = new boolean[N + 1];
-        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            sb.append(parent[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static int[] bfs(int n, List<List<Integer>> graph) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
+        int[] parent = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
+        
+        queue.add(1);
         visited[1] = true;
         
         while (!queue.isEmpty()) {
             int current = queue.poll();
-            for (int child : tree[current]) {
-                if (!visited[child]) {
-                    visited[child] = true;
-                    parent[child] = current;
-                    queue.offer(child);
+            for (int next : graph.get(current)) {
+                if (!visited[next]) {
+                    queue.add(next);
+                    visited[next] = true;
+                    parent[next] = current;
                 }
             }
         }
-        
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 2; i <= N; i++) {
-            bw.write(parent[i] + "\n");
-        }
-        bw.flush();
-        bw.close();
+        return parent;
     }
 }
