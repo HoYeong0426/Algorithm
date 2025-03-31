44a7cd0804686324fe844f4d4981d76bd70e3c2d
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-03-31 19:43:32 +0900

    [Silver I] Title: RGB거리, Time: 120 ms, Memory: 22380 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,28 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[][] cost = new int[N][3];
-        int[][] dp = new int[N][3];
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int score[][] = new int[N + 1][N + 1];
+        int dp[][] = new int[N + 1][N + 1];
+
 
         for (int i = 0; i < N; i++) {
-            cost[i][0] = sc.nextInt();
-            cost[i][1] = sc.nextInt();
-            cost[i][2] = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            score[i][0] = Integer.parseInt(st.nextToken());
+            score[i][1] = Integer.parseInt(st.nextToken());
+            score[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0][0] = cost[0][0];
-        dp[0][1] = cost[0][1];
-        dp[0][2] = cost[0][2];
+        dp[0][0] = score[0][0];
+        dp[0][1] = score[0][1];
+        dp[0][2] = score[0][2];
 
-        for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+        for (int i = 1; i <= N; i ++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + score[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + score[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + score[i][2];
         }
 
-        System.out.println(Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]));
+        System.out.println(Math.min(Math.min(dp[N][0], dp[N][1]), dp[N][2]));
+
     }
 }
