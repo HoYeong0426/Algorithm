971b9266c943a1972094ea36a1bca6b76bc12d56
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-04-08 16:59:59 +0900

    [Silver III] Title: 수리공 항승, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.java
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -1,26 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int L = sc.nextInt();
-        int[] leak = new int[N];
-        for (int i = 0; i < N; i++) {
-            leak[i] = sc.nextInt();
-        }
+    public static void main(String[] args) throws IOException {
 
-        Arrays.sort(leak);
-        int count = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
         double end = 0;
+        int count = 0;
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
 
         for (int i = 0; i < N; i++) {
-            if (leak[i] > end) {
+            if (arr[i] + 0.5 > end) {
                 count++;
-                end = leak[i] - 0.5 + L;
+                end = arr[i] - 0.5 + K;
             }
         }
-
         System.out.println(count);
+
     }
 }
