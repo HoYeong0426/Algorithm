92e8b90491c288ede651274604f832888632d626
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-04-17 16:26:03 +0900

    [level 2] Title: H-Index, Time: 0.91 ms, Memory: 93.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int length = citations.length;
+        
+        Arrays.sort(citations);
+        
+        for (int i = 0; i < length; i++) {
+            int h = length - i;
+            if (citations[i] >= h) {
+                return h;
+            }
+            
+        }
+        return 0;
+        
+        
+    }
+}
