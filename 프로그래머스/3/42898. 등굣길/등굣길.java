2666909e7af6f03bd7920e72251a83820904e306
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-04-24 19:28:52 +0900

    [level 3] Title: 등굣길, Time: 2.04 ms, Memory: 54 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n + 1][m + 1];
+        int MOD = 1_000_000_007;
+
+        for (int[] puddle : puddles) {
+            dp[puddle[1]][puddle[0]] = -1;
+        }
+
+        if (dp[1][1] == -1) return 0;
+        
+        dp[1][1] = 1;
+
+        for (int y = 1; y <= n; y++) {
+            for (int x = 1; x <= m; x++) {
+
+                if (dp[y][x] == -1) {
+                    dp[y][x] = 0;
+                    continue;
+                }
+
+                if (y > 1) dp[y][x] = (dp[y][x] + dp[y - 1][x]) % MOD;
+                if (x > 1) dp[y][x] = (dp[y][x] + dp[y][x - 1]) % MOD;
+            }
+        }
+
+        return dp[n][m];
+    }
+}
