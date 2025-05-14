898e551b1dbf732102dfcf6186b6baa331c90c60
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-05-14 15:26:54 +0900

    [Gold IV] Title: 플로이드, Time: 376 ms, Memory: 42720 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,38 +1,53 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] dist = new int[n + 1][n + 1];
-        final int INF = 100_000_000;
-        
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i], INF);
-            dist[i][i] = 0;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int city = Integer.parseInt(br.readLine());
+        int bus = Integer.parseInt(br.readLine());
+
+        int maxValue = 100_000_000;
+        int[][] map = new int[city + 1][city + 1];
+
+        for (int i = 1; i <= city; i++) {
+            Arrays.fill(map[i], maxValue);
+            map[i][i] = 0;
         }
-        
-        for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            dist[a][b] = Math.min(dist[a][b], c);
+
+        for (int i = 0; i < bus; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int price = Integer.parseInt(st.nextToken());
+
+            map[a][b] = Math.min(map[a][b], price);
         }
-        
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+
+
+        for (int k = 1; k <= city; k++) {
+            for(int i = 1; i <= city; i++) {
+                for(int j = 1; j <= city; j++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
                 }
             }
         }
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print((dist[i][j] == INF ? 0 : dist[i][j]) + " ");
+
+
+        for (int i = 1; i <= city; i++) {
+            for (int j = 1; j <= city; j++) {
+                sb.append(map[i][j] == maxValue ? 0 : map[i][j]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+
+        System.out.println(sb.toString());
+        
     }
 }
