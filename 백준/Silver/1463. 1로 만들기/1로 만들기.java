54218790a29bf06984a0a71dfa843157035221f3
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-06-10 15:40:19 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18152 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,34 +1,23 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
-public class Main {
-	
-    public static void main(String[] args) throws IOException {
-		
+class Main {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = 0;
 
-		int num = Integer.parseInt(br.readLine());
-		int[] dp = new int[num + 1];
-		dp[0] = dp[1] = 0;
-
-		for(int i = 2; i <= num; i++) {
-			dp[i] = dp[i - 1] + 1;
-			if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
-			if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
-		}
-
-		bw.write(String.valueOf(dp[num]));
-		
-		
-		br.close();
-		bw.close();
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
         
+        System.out.println(dp[N]);
     }
-	
-
 }
