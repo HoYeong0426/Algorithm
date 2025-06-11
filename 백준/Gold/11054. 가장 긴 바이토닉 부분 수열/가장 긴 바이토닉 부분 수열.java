e6347db0dfe49e9cb18502959bac6879f8f968d1
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-06-11 19:51:51 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 132 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,41 +1,48 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N];
+class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
         int[] inc = new int[N];
         int[] dec = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+            numbers[i] = Integer.parseInt(st.nextToken());
             inc[i] = 1;
             dec[i] = 1;
         }
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
-                if (A[j] < A[i]) {
-                    inc[i] = Math.max(inc[i], inc[j] + 1);
+                if (numbers[j] < numbers[i] && inc[i] < inc[j] + 1) {
+                    inc[i] = inc[j] + 1;
                 }
             }
         }
 
-        for (int i = N - 1; i >= 0; i--) {
+
+        for (int i = N - 2; i >= 0; i--) {
             for (int j = N - 1; j > i; j--) {
-                if (A[j] < A[i]) {
-                    dec[i] = Math.max(dec[i], dec[j] + 1);
+                if (numbers[j] < numbers[i] && dec[i] < dec[j] + 1) {
+                    dec[i] = dec[j] + 1;
                 }
             }
         }
 
-        int maxLen = 0;
+        int result = 0;
         for (int i = 0; i < N; i++) {
-            maxLen = Math.max(maxLen, inc[i] + dec[i] - 1);
+            result = Math.max(result, inc[i] + dec[i] - 1);
         }
 
-        System.out.println(maxLen);
-        sc.close();
+        System.out.println(result);
+
     }
 }
