d7e5941fbae389b5ad7664c52163620d9794d9e8
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-06-18 13:10:17 +0900

    [Silver II] Title: DFS와 BFS, Time: 280 ms, Memory: 25108 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -5,24 +5,24 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
+    static int N;
     static int[][] map;
     static boolean[] visited;
-    static int N;
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int count = Integer.parseInt(st.nextToken());
         int start = Integer.parseInt(st.nextToken());
 
         map = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
 
-        for (int i = 0 ; i < K; i++) {
+        for (int i = 0; i < count; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
@@ -31,45 +31,45 @@ public class Main {
             map[b][a] = 1;
         }
 
-        visited = new boolean[N + 1];
         dfs(start);
-        
+
         System.out.println();
 
         visited = new boolean[N + 1];
+
         bfs(start);
 
     }
 
-    static void dfs(int start) {
-        System.out.print(start + " ");
-        visited[start] = true;
+    static void dfs(int num) {
+        
+        System.out.print(num + " ");
+        visited[num] = true;
 
-        for (int i = 0; i < map.length; i++) {
-            if (map[start][i] != 1 || visited[i]) continue;
-            dfs(i);
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && map[num][i] == 1) {
+                dfs(i);
+            }
         }
-
     }
 
-    static void bfs(int start) {
+    static void bfs(int num) {
         Queue<Integer> queue = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
-        queue.add(start);
-        visited[start] = true;
-        sb.append(start).append(" ");
-
+        visited[num] = true;
+        queue.offer(num);
+        sb.append(num).append(" ");
         while(!queue.isEmpty()) {
             int now = queue.poll();
 
-            for (int i = 0; i < map.length; i++) {
-                if (map[now][i] != 1 || visited[i]) continue;
-                visited[i] = true;
-                queue.offer(i);
-                sb.append(i).append(" ");
+            for (int i = 1; i <= N; i++) {
+                if (!visited[i] && map[now][i] == 1) {
+                    visited[i] = true;
+                    queue.offer(i);
+                    sb.append(i).append(" ");
+                }
             }
         }
-
         System.out.println(sb.toString());
     }
 
