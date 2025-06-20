50f87dfb0219fcf1734da991b27f46ec3d042015
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-06-20 16:06:24 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -5,14 +5,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
     static int[][] map;
     static boolean[][] visited;
+    static int[] dx = new int[] {-1, 1, 0, 0};
+    static int[] dy = new int[] {0, 0, -1, 1};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,24 +24,23 @@ public class Main {
         visited = new boolean[x][y];
 
         for (int i = 0; i < x; i++) {
-            String str = br.readLine();
+            String numbers = br.readLine();
             for (int j = 0; j < y; j++) {
-                map[i][j] = str.charAt(j) - '0';
+                map[i][j] = (int) numbers.charAt(j) - '0';
             }
         }
 
-        visited[0][0] = true;
-
         bfs(0, 0);
 
-        System.out.println(map[x -1][y-1]);
+        System.out.println(map[x - 1][y - 1]);
+
     }
 
     static void bfs(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[] {x, y});
-
-        while (!queue.isEmpty()) {
+        
+        while(!queue.isEmpty()) {
             int[] arr = queue.poll();
             int nowX = arr[0];
             int nowY = arr[1];
@@ -49,16 +48,16 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int nextX = nowX + dx[i];
                 int nextY = nowY + dy[i];
-                if (nextX < 0 || nextY < 0 || nextX >= map.length || nextY >= map[0].length) continue;
+
+                if (nextX < 0 || nextX >= map.length || nextY < 0 || nextY >= map[0].length) continue;
                 if (visited[nextX][nextY] || map[nextX][nextY] == 0) continue;
 
-                queue.offer(new int[] {nextX, nextY});
+                queue.add(new int[] {nextX, nextY});
                 visited[nextX][nextY] = true;
                 map[nextX][nextY] = map[nowX][nowY] + 1;
             }
             
         }
-
 
     }
 
