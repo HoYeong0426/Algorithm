3abdb9071384a8491241e64b7cc3e6fa71eb3325
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-03 18:27:51 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 132 ms, Memory: 15172 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -5,41 +5,43 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
-
     public static void main(String[] args) throws IOException {
-
+        // 6
+        // 10 20 10 30 20 50
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] numbers = new int[N];
-        int[] dp = new int[N];
-        int[] prev = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        int[] prev = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
             dp[i] = 1;
             prev[i] = -1;
         }
 
         int maxLength = 1;
         int lastIndex = 0;
-        for (int i = 1; i < N; i++) {
+
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (numbers[i] > numbers[j] && dp[i] < dp[j] + 1) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
                     dp[i] = dp[j] + 1;
                     prev[i] = j;
                 }
-            }
-            if (dp[i] > maxLength) {
-                maxLength = dp[i];
-                lastIndex = i;
+
+                if (dp[i] > maxLength) {
+                    maxLength = dp[i];
+                    lastIndex = i;
+                }
             }
         }
 
         Stack<Integer> stack = new Stack<>();
-        while (lastIndex != -1) {
-            stack.push(numbers[lastIndex]);
+        for (int i = 0; i < maxLength; i++) {
+            stack.push(arr[lastIndex]);
             lastIndex = prev[lastIndex];
         }
 
@@ -49,5 +51,4 @@ class Main {
         }
 
     }
-
 }
