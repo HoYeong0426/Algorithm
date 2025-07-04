4efdedfd85674c80716f960e5e61e961d8c91d91
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-04 16:51:14 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 620 ms, Memory: 133340 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(list, arr[i]);
+
+            if (idx == list.size()) {
+                list.add(arr[i]);
+            } else {
+                list.set(idx, arr[i]);
+            }
+
+        }
+
+        System.out.println(list.size());
+
+    }
+
+    public static int binarySearch(List<Integer> list, int target) {
+        int left = 0;
+        int right = list.size();
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (list.get(mid) >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+
+}
