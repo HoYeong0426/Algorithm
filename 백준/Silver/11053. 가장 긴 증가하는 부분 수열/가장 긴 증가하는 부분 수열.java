bb9e1001991240cacd382e4eb19042a2701bb33b
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-08 14:47:15 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 112 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,38 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        List<Integer> list = new ArrayList<>();
 
-        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[N];
-        int[] dp = new int[N];
-
-        for (int i = 0; i < N; i ++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;
         }
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                }
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(list, arr[i]);
 
+            if (idx == list.size()) {
+                list.add(arr[i]);
+            } else {
+                list.set(idx, arr[i]);
+            }
+
+        }
+
+        System.out.println(list.size());
+        
+    }
+
+    public static int binarySearch(List<Integer> list, int target) {
+        int left = 0;
+        int right = list.size();
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (list.get(mid) >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
 
-        int result = 0;
-        for (int i = 0; i < N; i++) {
-            result = Math.max(result, dp[i]);
-        }
-
-        System.out.println(result);
-
+        return left;
     }
+
 }
