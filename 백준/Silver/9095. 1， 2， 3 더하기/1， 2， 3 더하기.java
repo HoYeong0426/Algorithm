a7b7d5d27b32625131a29d6cb8a0088592089517
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-11 11:17:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -2,16 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
+public class Main {
 
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
 
-        for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
+
             int[] dp = new int[Math.max(4, num + 1)];
 
             dp[1] = 1;
@@ -19,13 +20,12 @@ class Main {
             dp[3] = 4;
 
             for (int j = 4; j <= num; j++) {
-                dp[j] = dp[j - 3] + dp[j - 2] + dp[j -1];
+                dp[j] = (dp[j - 3] + dp[j - 2] + dp[j - 1]) % 10007;
             }
+
             System.out.println(dp[num]);
+
         }
-
-
-
     }
 
 }
