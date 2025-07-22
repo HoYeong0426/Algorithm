e56a645a95fac88a3be73bfb900510d16235c29f
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-22 15:17:37 +0900

    [Silver II] Title: 외판원 순회 2, Time: 280 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -6,51 +6,49 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int n;
-    static int[][] arr;
+    static int[][] map;
     static boolean[] visited;
-    static int result = Integer.MAX_VALUE;
-    
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         n = Integer.parseInt(br.readLine());
 
-        arr = new int[n][n];
+        map = new int[n][n];
         visited = new boolean[n];
+
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
 
         for (int i = 0; i < n; i++) {
             visited[i] = true;
-            dfs(i, i, 0 , 0);
+            bfs(i, 0, 0, i);
             visited[i] = false;
         }
 
-        System.out.println(result);
+        System.out.println(min);
 
     }
 
-    public static void dfs(int start, int now, int count, int sum) {
+    public static void bfs(int idx, int count, int sum, int start) {
 
         if (count == n - 1) {
-            if (arr[now][start] != 0) {
-                result = Math.min(result, sum + arr[now][start]);
+            if (map[idx][start] != 0) {
+                min = Math.min(min, sum + map[idx][start]);
             }
+            return;
         }
 
-
-        for (int next = 0; next < n; next++) {
-            if (visited[next] || arr[now][next] == 0) continue;
-            visited[next] = true;
-            dfs(start, next, count + 1, sum + arr[now][next]);
-            visited[next] = false;
+        for (int i = 0; i < n; i++) {
+            if (visited[i] || map[idx][i] == 0) continue;
+            visited[i] = true;
+            bfs(i, count + 1, sum + map[idx][i], start);
+            visited[i] = false;
         }
 
     }
