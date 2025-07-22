fe5a94563056c6216f972b08c1a9fdcc7eb82a2d
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-22 14:20:37 +0900

    [Silver I] Title: 스타트와 링크, Time: 372 ms, Memory: 19752 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,55 +1,66 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[][] S;
+
+    static int n;
+    static int[][] map;
     static boolean[] visited;
     static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        S = new int[N][N];
-        visited = new boolean[N];
+    public static void main(String[] args) throws IOException {
 
-        // 능력치 행렬 입력
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                S[i][j] = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n][n];
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
         dfs(0, 0);
 
         System.out.println(min);
+
     }
 
-    static void dfs(int depth, int idx) {
-        if (depth == N / 2) {
-            calcDiff();
-            return;
-        }
+    public static void dfs(int depth, int idx) {
 
-        for (int i = idx; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(depth + 1, i + 1);
-                visited[i] = false;
-            }
-        }
-    }
+        if (depth == n / 2) {
+            int teamA = 0;
+            int teamB = 0;
 
-    static void calcDiff() {
-        int start = 0, link = 0;
-
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (visited[i] && visited[j]) {
-                    start += S[i][j] + S[j][i];
-                } else if (!visited[i] && !visited[j]) {
-                    link += S[i][j] + S[j][i];
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (visited[i] && visited[j]) {
+                        teamA += map[i][j] + map[j][i];
+                    }
+                    if (!visited[i] && !visited[j]) {
+                        teamB += map[i][j] + map[j][i];
+                    }
                 }
             }
+
+            min = Math.min(min, Math.abs(teamA - teamB));
+            return;
+
         }
 
-        min = Math.min(min, Math.abs(start - link));
+        for (int i = idx; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            dfs(depth + 1, i + 1);
+            visited[i] = false;
+        }
+
+
     }
+
 }
