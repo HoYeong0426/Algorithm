7bd97f94689720f30210c999eb141bec2f4e183a
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-28 11:14:23 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,50 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
-
-    static int[] hobbit;
-    static List<Integer> list;
-    static boolean found = false;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        hobbit = new int[9];
+        int[] hobbit = new int[9];
+        int sum = 0;
         for (int i = 0; i < 9; i++) {
             hobbit[i] = Integer.parseInt(br.readLine());
+            sum += hobbit[i];
         }
 
         Arrays.sort(hobbit);
 
-        dfs(0, 0, new ArrayList<>());
-
-        for (int i : list) {
-            System.out.println(i);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == j) continue;
+                if (sum - hobbit[i] - hobbit[j] == 100) {
+                    for (int k = 0; k < 9; k++) {
+                        if (k == i || k == j) continue;
+                        System.out.println(hobbit[k]);
+                    }
+                    return;
+                }
+            }
         }
 
     }
-
-    public static void dfs(int n, int weight, List<Integer> result) {
-        if (found) return;
-
-        if (result.size() == 7 && weight == 100) {
-            list = new ArrayList<>(result);
-            found = true;
-            return;
-        }
-        
-        for (int i = n; i < 9; i++) {
-            result.add(hobbit[i]);
-            dfs(i + 1, weight + hobbit[i], result);
-            result.remove(result.size() - 1);
-        }
-
-    }
-
 }
