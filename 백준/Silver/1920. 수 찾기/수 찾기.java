838a9f7978fd180d5acfe5c1c3896028af742880
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-28 19:50:09 +0900

    [Silver IV] Title: 수 찾기, Time: 1044 ms, Memory: 54300 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,44 +1,60 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
+    static int num;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
-		
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int count = Integer.parseInt(br.readLine());
-		int[] arr = new int[count];
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < count; i++) arr[i] = Integer.parseInt(st.nextToken());
+        num = Integer.parseInt(br.readLine());
+        arr = new int[num];
 
-		int count2 = Integer.parseInt(br.readLine());
-		int[] arr2 = new int[count2];
-		int[] result = new int[count2];
-		for(int i = 0; i < count2; i++) result[i] = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < num; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < count2; i++) arr2[i] = Integer.parseInt(st.nextToken());
-
-		for(int i = 0; i < count2; i++) {
-			for(int j = 0; j < count; j++) {
-				if(arr2[i] == arr[j]) {
-					result[i] = 1;
-					break;
-				} 
-			}
-		}
-		
-		for (int i : result) bw.write(String.valueOf(i) + "\n");
-
-		br.close();
-		bw.close();
+        Arrays.sort(arr);
         
+        int num2 = Integer.parseInt(br.readLine());
+        int[] arr2 = new int[num2];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < num2; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < num2; i++) {
+            System.out.println(isContains(arr2[i]));
+        }
+
+
     }
+
+    public static int isContains(int n) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2; 
+
+            if (arr[mid] == n) {
+                return 1;
+            } else if (arr[mid] >= n) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return 0;
+    }
+
 }
