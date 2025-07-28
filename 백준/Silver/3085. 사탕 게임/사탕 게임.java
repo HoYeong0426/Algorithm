652dea6256ed9e2398e7ae686692d8ae1bfcd56e
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-28 13:56:00 +0900

    [Silver II] Title: 사탕 게임, Time: 140 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -5,44 +5,41 @@ import java.io.InputStreamReader;
 public class Main {
 
     static int n;
-    static int max = 1;
     static char[][] arr;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        n = Integer.parseInt(br.readLine());
-        arr = new char[n][n];
 
-        for (int i = 0; i < n; i++) {
+        n = Integer.parseInt(br.readLine());
+        arr =new char[n][n];
+
+        for(int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < str.length(); j++) {
                 arr[i][j] = str.charAt(j);
             }
         }
+        
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
 
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                
-                if (col < n - 1) {
-                    swap(row, col, row, col + 1);
-                    count();
-                    swap(row, col, row, col + 1);
-                }
+                swap(i, j, i, j + 1);
+                count = Math.max(count, count());
+                swap(i, j, i, j + 1);
 
-
-                if (row < n - 1) {
-                    swap(row, col, row + 1, col);
-                    count();
-                    swap(row, col, row + 1, col);
-                }
+                swap(j, i, j + 1, i);
+                count = Math.max(count, count());
+                swap(j, i, j + 1, i);
 
             }
         }
 
-        System.out.println(max);
+        System.out.println(count);
+
     }
+
 
     public static void swap(int row, int col, int row2, int col2) {
         char temp = arr[row][col];
@@ -50,32 +47,26 @@ public class Main {
         arr[row2][col2] = temp;
     }
 
-    public static void count() {
+    public static int count() {
+        int max = 1;
 
-        for (int col = 0; col < n; col++) {
-            int count = 1;
-            for (int row = 1; row < n; row++) {
-                if (arr[row][col] == arr[row - 1][col]) {
-                    count++;
-                } else {
-                    count = 1;
-                }
-                max = Math.max(max, count);
+        for (int i = 0; i < n; i++) {
+            int rowCount = 1;
+            int colCount = 1;
+
+            for (int j = 1; j < n; j++) {
+                if (arr[j][i] == arr[j - 1][i]) colCount++;
+                else colCount = 1;
+
+                if (arr[i][j] == arr[i][j - 1]) rowCount++;
+                else rowCount = 1;
+
+                max = Math.max(max, Math.max(rowCount, colCount));
             }
+
         }
 
-        for (int row = 0; row < n; row++) {
-            int count = 1;
-            for (int col = 1; col < n; col++) {
-                if (arr[row][col] == arr[row][col - 1]) {
-                    count++;
-                } else {
-                    count = 1;
-                }
-                max = Math.max(max, count);
-            }
-        }
-
+        return max;
     }
 
 }
