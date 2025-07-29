07e332ddedfaedc00817b91df992a6dbbc86df7e
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-07-29 11:43:56 +0900

    [Silver II] Title: 외판원 순회 2, Time: 264 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -11,13 +11,12 @@ public class Main {
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
 
+        n = Integer.parseInt(br.readLine());
+        
         map = new int[n][n];
         visited = new boolean[n];
-
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
@@ -27,7 +26,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             visited[i] = true;
-            bfs(i, 0, 0, i);
+            dfs(0, i, i, 0);
             visited[i] = false;
         }
 
@@ -35,19 +34,19 @@ public class Main {
 
     }
 
-    public static void bfs(int idx, int count, int sum, int start) {
-
-        if (count == n - 1) {
-            if (map[idx][start] != 0) {
-                min = Math.min(min, sum + map[idx][start]);
+    public static void dfs(int depth, int idx, int first, int sum) {
+        if (depth == n - 1) {
+            if (map[idx][first] != 0) {
+                min = Math.min(min, sum + map[idx][first]);
+                return;
             }
-            return;
+            
         }
 
         for (int i = 0; i < n; i++) {
             if (visited[i] || map[idx][i] == 0) continue;
             visited[i] = true;
-            bfs(i, count + 1, sum + map[idx][i], start);
+            dfs(depth + 1, i, first, sum + map[idx][i]);
             visited[i] = false;
         }
 
