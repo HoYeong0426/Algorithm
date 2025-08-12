776edf5c7ef60c26e3fd793230e27045677ea5a9
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-08-12 09:08:52 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 984 ms, Memory: 172816 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,49 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-	
     public static void main(String[] args) throws IOException {
-		
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 
-		int num = Integer.parseInt(br.readLine());
-		int[] arr = new int[num];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < num; i++) arr[i] = Integer.parseInt(st.nextToken());
-		
-		int num2 = Integer.parseInt(br.readLine());
-		int[] arr2 = new int[num2];
-		int[] arr3 = new int[num2];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < num2; i++) {
-			arr2[i] = Integer.parseInt(st.nextToken());
-			arr3[i] = arr2[i];
-		}
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
 
-		Arrays.sort(arr2);
-		Map<Integer, Integer> map = new HashMap<>();
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            sb.append(map.getOrDefault(Integer.parseInt(st.nextToken()), 0)).append(" ");
+        }
 
-		for(int i = 0; i < num; i++) {
-			if(Arrays.binarySearch(arr2, arr[i]) >= 0) map.put(arr[i], map.getOrDefault(arr[i], 0) + 1) ;
-		}
+        System.out.println(sb.toString());
 
-		for(int i = 0; i < num2; i++) {
-			bw.write(String.valueOf(map.getOrDefault(arr3[i], 0)) + " ");
-		}
-
-		br.close();
-		bw.close();
-        
     }
-
 }
