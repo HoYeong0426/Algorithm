f31543ac4df80ad28b29e088a93950cd3360d684
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-08-18 18:33:09 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 17440 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,54 +1,50 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
+    static int N,M;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
-		
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        long[] lanCables = new long[K];
-
-        long maxLen = 0;
-        for (int i = 0; i < K; i++) {
-            lanCables[i] = Long.parseLong(br.readLine());
-            maxLen = Math.max(maxLen, lanCables[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        long left = 1;
+        long right = 0;
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            right = Math.max(right, arr[i]);
         }
 
-        long left = 1;
-        long right = maxLen;
-        long answer = 0;
 
+        long result = 0;
         while (left <= right) {
             long mid = (left + right) / 2;
-            long count = 0;
 
-            for (int i = 0; i < K; i++) {
-                count += lanCables[i] / mid;
-            }
-
-            if (count >= N) {
-                answer = mid;
+            if (isPossible(mid)) {
+                result = mid;
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
 
-		bw.write(String.valueOf(answer));
+        System.out.println(result);
 
-		br.close();
-		bw.close();
-        
+    }
+
+    public static boolean isPossible(long target) {
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            count += arr[i] / target;
+        }
+
+        return count >= M;
     }
 
 }
