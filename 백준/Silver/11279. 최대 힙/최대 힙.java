2cce8b7005dd5cc795e6317c6760bc93af28938d
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-08-18 19:51:59 +0900

    [Silver II] Title: 최대 힙, Time: 272 ms, Memory: 26312 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,29 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < N; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if(x == 0) {
-				if(pq.isEmpty()) sb.append(0).append("\n");
-				else {
-					sb.append(pq.poll()).append("\n");
-				}
-			}else {
-				pq.offer(x);
-			}
-		}
-		System.out.println(sb);
-	}
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq =  new PriorityQueue<>(Comparator.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+                continue;
+            } 
+            pq.offer(num);
+        }
+
+        System.out.println(sb.toString());
+
+    }
+
 
 }
