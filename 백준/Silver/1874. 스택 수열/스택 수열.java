9d0f33d649716db044bcda9546b0e56a5fa7dd88
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-08-21 18:38:29 +0900

    [Silver II] Title: 스택 수열, Time: 300 ms, Memory: 28440 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,50 +1,47 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
-	
+
+
     public static void main(String[] args) throws IOException {
-		
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		
-		int count = Integer.parseInt(br.readLine());
-		int[] arr = new int[count];
-		for(int i = 0; i < count; i++) arr[i] = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-
-		int num = 1;
-		for(int i = 0; i < count; i++) {
-			
-			while(num <= arr[i]) {
-				stack.push(num);
-				sb.append("+").append("\n");
-				num++;
-			}
-
-			if(stack.peek() == arr[i]) {
-				stack.pop();
-				sb.append("-").append("\n");
-			} else {
-				sb = new StringBuilder();
-				sb.append("NO");
-				break;
-			}
-
-		}
-
-		bw.write(sb.toString());
-		
-
-		br.close();
-		bw.close();
         
-    }
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
+        int num = 1;
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int now = arr[i];
+
+            if (now >= num) {
+                while (now >= num) {
+                    stack.push(num++);
+                    sb.append("+").append("\n");
+                }
+                stack.pop();
+                sb.append("-").append("\n");
+            } else {
+                int pop = stack.pop();
+                if (pop == now) {
+                    sb.append("-").append("\n");
+                } else {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+
+        }
+
+        System.out.println(sb.toString());
+
+    }
 }
