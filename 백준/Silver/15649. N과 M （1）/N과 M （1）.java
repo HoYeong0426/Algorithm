795902168902b00d6858c8451b5052fde49026ca
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-09-01 18:34:35 +0900

    [Silver III] Title: N과 M (1), Time: 280 ms, Memory: 34216 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,38 +1,51 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static boolean[] visited;
+
+    static int N,M;
     static int[] result;
+    static boolean[] visited;
+    static StringBuilder sb;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        sc.close();
+    public static void main(String[] args) throws IOException {
 
-        visited = new boolean[N + 1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
         result = new int[M];
+        visited = new boolean[N + 1];
+        sb = new StringBuilder();
+
+        dfs(0);
         
-        backtrack(0);
+        System.out.println(sb.toString());
     }
 
-    static void backtrack(int depth) {
+    static void dfs(int depth) {
+
         if (depth == M) {
-            for (int num : result) {
-                System.out.print(num + " ");
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i] + " ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
 
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                result[depth] = i;
-                backtrack(depth + 1);
-                visited[i] = false;
-            }
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            result[depth] = i;
+            dfs(depth + 1);
+            visited[i] = false;
+
         }
+
     }
 }
