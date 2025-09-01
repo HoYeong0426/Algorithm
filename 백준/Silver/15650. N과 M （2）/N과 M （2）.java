599178666d3f3f0d1d9edfcb206752d3884ce028
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-09-01 19:02:14 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,29 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[] selected;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        selected = new int[M];
+    static int N,M;
+    static int[] result;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        result = new int[M];
+        sb = new StringBuilder();
+
         dfs(1, 0);
+        
+        System.out.println(sb.toString());
     }
 
     static void dfs(int start, int depth) {
+
         if (depth == M) {
             for (int i = 0; i < M; i++) {
-                System.out.print(selected[i] + " ");
+                sb.append(result[i] + " ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
 
         for (int i = start; i <= N; i++) {
-            selected[depth] = i;
+            result[depth] = i;
             dfs(i + 1, depth + 1);
         }
+
     }
 }
