ceac4061893941ccef2cf9db16f591c5f6437342
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-09-02 16:07:01 +0900

    [Silver III] Title: 소수 구하기, Time: 468 ms, Memory: 21636 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -4,30 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    
+
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int first = Integer.parseInt(st.nextToken());
-        int last = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        for (int i = first; i <= last; i++ ) {
-            if (i < 2) continue;
-            boolean result = true;
-
-            for (int j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    result = false;
-                    break;
-                }
-            }
-            if (result) System.out.println(i);
+        StringBuilder sb = new StringBuilder();
+        for (int i = N; i <= M; i++) {
+            if (isPrime(i)) sb.append(i + "\n");
         }
+
+        System.out.println(sb.toString());
 
     }
 
-    
+    static boolean isPrime(int target) {
+        if (target < 2) return false;
+
+        for (int i = 2; i <= Math.sqrt(target); i++) {
+            if (target % i == 0) return false;
+        }
+
+
+        return true;
+    }
+
 }
