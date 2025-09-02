912bcda6e8865740b02fcf62236dad9236c9b1e0
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-09-02 11:42:44 +0900

    [Gold IV] Title: N-Queen, Time: 5128 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,42 +4,47 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int N, count;
+    static int N;
     static int[] board;
+    static int count;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
-
         board = new int[N];
-        count = 0;
 
-        solve(0);
+        dfs(0);
+
         System.out.println(count);
 
     }
 
-    static void solve(int row) {
-        if (row == N) {
+    static void dfs(int depth) {
+
+        if (depth == N) {
             count++;
             return;
         }
 
-        for (int col = 0; col < N; col++) {
-            if (isSafe(row, col)) {
-                board[row] = col;
-                solve(row + 1);
+        for (int i = 0; i < N; i++) {
+
+            if (isCorrect(depth, i)) {
+                board[depth] = i;
+                dfs(depth + 1);
             }
+            
         }
+
     }
 
-    static boolean isSafe(int row, int col) {
-        for (int i = 0; i < row; i ++) {
-            if (board[i] == col || Math.abs(board[i] - col) == row - i) {
-                return false;
-            }
+    static boolean isCorrect(int row, int col) {
+
+        for (int i = 0; i < row; i++) {
+            if (board[i] == col || Math.abs(col - board[i]) == row - i) return false;
         }
+
         return true;
     }
 }
