817bf87951d094c1fd328ac310cf3e7f6db2f7b0
HoYeong0426 <116083222+HoYeong0426@users.noreply.github.com>
2025-09-05 16:16:03 +0900

    [Gold III] Title: 줄 세우기, Time: 464 ms, Memory: 47136 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] graph = new ArrayList[N + 1];
+        int[] indegree = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            indegree[b]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) queue.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            sb.append(now + " ");
+            
+            for (int n : graph[now]) {
+                indegree[n]--;
+                if(indegree[n] == 0) queue.offer(n);
+            }
+
+        }
+
+        System.out.println(sb.toString());
+
+
+    }
+
+
+}
